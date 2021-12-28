9c37d9dbcab71d900e7980e0c7b0826a288ece60
tomihcev <tomicev@gmail.com>
2021-12-28 16:39:52 +0200

    owl people

---
+++ js/sсripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin : 10,
+        nav: true,
+        navText: ['', ' '],
+
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            1000: {
+                items: 3
+            }
+        }
+    });
+});
